9d4bb4123c944d6458717cd47d5a819ea6730f7b
Siar Wahidi <siar.wahidi@gmail.com>
2024-06-26 19:10:18 -0500

    Displaying data images using interval func

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,37 +1,48 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const data = [
+  {
+    id: 1,
+    title: "always hot, fresh & ready",
+    image: "/slide1.png"
+  },
+  {
+    id: 2,
+    title: "we deliver anywhere within the GTA",
+    image: "/slide2.png"
+  },
+  {
+    id: 3,
+    title: "Best shared with loved ones",
+    image: "/slide3.jpg"
+  },
+]
 
 const Slider = () => {
 
-  const data = [
-    {
-      id: 1,
-      title: "always hot, fresh & ready",
-      image: "/slide1.png"
-    },
-    {
-      id: 2,
-      title: "we deliver anywhere within the GTA",
-      image: "/slide2.png"
-    },
-    {
-      id: 3,
-      title: "Best shared with loved ones",
-      image: "/slide3.png"
-    },
-  ]
+  const [currentSlide, setCurrentSlide] = useState(0)
+ 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide(prevState => prevState === data.length - 1 ?  0 : prevState + 1);
+    }, 2000);
 
+    return () => clearInterval(interval)
+  }, [currentSlide])
 
   return (
     <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
       <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
         <h1 className='text-5xl text-center uppercase p-4'>
-          Test
+          {data[currentSlide].title}
         </h1>
         <button className='pointer bg-red-500 text-white py-4 px-8 rounded-md md:text-6xl xl:text-7xl'>Order Now</button>
       </div>
       <div className='w-full h-1/2 relative'>
-        <Image src="/slide1.png" alt="holding pizza" fill className='object-cover'/>
+        <Image src={data[currentSlide].image} alt="holding pizza" fill className='object-cover'/>
       </div>
     </div>
   )
