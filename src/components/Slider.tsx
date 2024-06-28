97f5f9bb71c91a8016e0531bca10e0f4d67d2734
Siar Wahidi <siar.wahidi@gmail.com>
2024-06-27 19:01:12 -0500

    Added some more responsive styling to slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -34,14 +34,14 @@ const Slider = () => {
   }, [currentSlide])
 
   return (
-    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
-      <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
-        <h1 className='text-5xl text-center uppercase p-4'>
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
+      <div className='flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
+        <h1 className='text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl'>
           {data[currentSlide].title}
         </h1>
-        <button className='pointer bg-red-500 text-white py-4 px-8 rounded-md md:text-6xl xl:text-7xl'>Order Now</button>
+        <button className='pointer bg-red-500 text-white py-4 px-8 rounded-md'>Order Now</button>
       </div>
-      <div className='w-full h-1/2 relative'>
+      <div className='w-full flex-1 relative'>
         <Image src={data[currentSlide].image} alt="holding pizza" fill className='object-cover'/>
       </div>
     </div>
